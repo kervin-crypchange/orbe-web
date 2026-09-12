0068b3db21f81dd1a4ab2e322c482b8dbc050c57
Kervin Gonzalez <kervin.gonzalez@crypchange.com>
2026-09-12 10:42:37 -0400

    comentarios

--- src/app/core/landing/sections/comments/comments.ts
+++ src/app/core/landing/sections/comments/comments.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './comments.html',
   styleUrl: './comments.css',
 })
-export class Comments {}
+export class Comments {
+  commensts = [
+    {
+      avatar:'https://cdn.flyonui.com/fy-assets/avatar/avatar-1.png',
+      comment:'Encontre una asesora con la que conecté desde el primer momento. La experiencia fia increible.',
+      name:'Laura M.'
+    },
+    {
+      avatar:'https://cdn.flyonui.com/fy-assets/avatar/avatar-1.png',
+      comment:'Encontre una asesora con la que conecté desde el primer momento. La experiencia fia increible.',
+      name:'Laura M.'
+    },
+    {
+      avatar:'https://cdn.flyonui.com/fy-assets/avatar/avatar-1.png',
+      comment:'Encontre una asesora con la que conecté desde el primer momento. La experiencia fia increible.',
+      name:'Laura M.'
+    },
+  ]
+}
